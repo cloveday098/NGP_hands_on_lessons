7f5d944bf05f2b86e6b2d404bc23cbbb8b1d8dd4
Chance B. Loveday <141525952+cloveday098@users.noreply.github.com>
2026-07-09 12:18:51 -0400

    Added premade Python scripts from lesson

---
+++ intro_to_python/loops/while_loops.py
@@ -0,0 +1,20 @@
+# while_loops.py
+
+
+# iterate only while "a" is less than "b"
+print('Example 6.5: while a<b')
+
+a = 0 
+b = 5
+while a<b:
+    print('a equals',a)
+    a = a + 1
+
+# iterate until "c" equals 6
+print(' ')
+print('Example 6.6: while c!=6')
+
+c = 0
+while c!=6:
+    print("Am I 6 yet?", "Nope, I am", c)
+    c = c + 1
